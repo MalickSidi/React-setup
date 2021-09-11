06aef70f72a512f4f5c78987f7d402de2ee2bcc8
Malick S <malick.s.mohamed@gmail.com>
2021-09-11 09:04:49 +0300

    React setup ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import image from "./asset/img.png";
-import { Counter } from "./components/Counter";
 
 const App = () => {
   return (
     <>
       <div className="main">
-        <h1>Hello</h1>
-        <Counter />
+        <h1>Start Kit</h1>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia
           dolores consequuntur totam! Eius, sequi qui excepturi, modi dolore
@@ -15,10 +12,20 @@ const App = () => {
           perferendis sint sequi.
         </p>
       </div>
-      <div className="">
-        <h3>Buy Menu</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-        <img src={image} alt="" />
+      <div className="card">
+        <div className="">
+          <h3>Some Title</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+          <p>
+            Sit amet consectetur Lorem, ipsum dolor sit amet consectetur
+            adipisicing.
+          </p>
+          <p>Ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+        <img
+          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+          alt="logo"
+        />
       </div>
     </>
   );
--- step.sh
+++ step.sh
@@ -11,3 +11,8 @@ npm i -D @babel/core @babel/preset-env @babel/preset-react @babel/preset-typescr
 touch babel.config.json
 npm i -D webpack webpack-cli webpack-dev-server html-webpack-plugin babel-loader webpack-merge
 npm i -D css-loader style-loader sass-loader sass
+#npm i -D eslint-plugin-react eslint-plugin-hooks
+#npm i -D @typescript-eslint/parser @typescript-eslint/eslint-plugin
+npx eslint --init
+npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+npx prettier-init
